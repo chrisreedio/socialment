2f153a0e8f8434b94975d178c349beb9b4455702
Chris Reed <chris@reedtech.us>
2023-08-28 17:37:44 -0500

    Should fix a few PHPStan issues

--- src/SocialmentServiceProvider.php
+++ src/SocialmentServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace ChrisReedIO\Socialment;
 
-use ChrisReedIO\Socialment\Commands\SocialmentCommand;
 use ChrisReedIO\Socialment\Testing\TestsSocialment;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
@@ -18,138 +17,136 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class SocialmentServiceProvider extends PackageServiceProvider
 {
-    public static string $name = 'socialment';
+	public static string $name = 'socialment';
 
-    public static string $viewNamespace = 'socialment';
+	public static string $viewNamespace = 'socialment';
 
-    public function configurePackage(Package $package): void
-    {
-        /*
+	public function configurePackage(Package $package): void
+	{
+		/*
          * This class is a Package Service Provider
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
-        $package->name(static::$name)
-            // ->hasCommands($this->getCommands())
-            ->hasRoute('web')
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('chrisreedio/socialment');
-            });
+		$package->name(static::$name)
+			// ->hasCommands($this->getCommands())
+			->hasRoute('web')
+			->hasInstallCommand(function (InstallCommand $command) {
+				$command
+					->publishConfigFile()
+					->publishMigrations()
+					->askToRunMigrations()
+					->askToStarRepoOnGitHub('chrisreedio/socialment');
+			});
 
-        $configFileName = $package->shortName();
+		$configFileName = $package->shortName();
 
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
+		if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
+			$package->hasConfigFile();
+		}
 
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
+		if (file_exists($package->basePath('/../database/migrations'))) {
+			$package->hasMigrations($this->getMigrations());
+		}
 
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
+		if (file_exists($package->basePath('/../resources/lang'))) {
+			$package->hasTranslations();
+		}
 
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
-    }
+		if (file_exists($package->basePath('/../resources/views'))) {
+			$package->hasViews(static::$viewNamespace);
+		}
+	}
 
-    public function packageRegistered(): void
-    {
-    }
+	public function packageRegistered(): void
+	{
+	}
 
-    public function packageBooted(): void
-    {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
+	public function packageBooted(): void
+	{
+		// Asset Registration
+		FilamentAsset::register(
+			$this->getAssets(),
+			$this->getAssetPackageName()
+		);
 
-        // FilamentAsset::registerScriptData(
-        //     $this->getScriptData(),
-        //     $this->getAssetPackageName()
-        // );
+		// FilamentAsset::registerScriptData(
+		//     $this->getScriptData(),
+		//     $this->getAssetPackageName()
+		// );
 
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
+		// Icon Registration
+		FilamentIcon::register($this->getIcons());
 
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/socialment/{$file->getFilename()}"),
-                ], 'socialment-stubs');
-            }
-        }
+		// Handle Stubs
+		if (app()->runningInConsole()) {
+			foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
+				$this->publishes([
+					$file->getRealPath() => base_path("stubs/socialment/{$file->getFilename()}"),
+				], 'socialment-stubs');
+			}
+		}
 
-        // Testing
-        Testable::mixin(new TestsSocialment());
-    }
+		// Testing
+		Testable::mixin(new TestsSocialment());
+	}
 
-    protected function getAssetPackageName(): ?string
-    {
-        return 'chrisreedio/socialment';
-    }
+	protected function getAssetPackageName(): ?string
+	{
+		return 'chrisreedio/socialment';
+	}
 
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('socialment', __DIR__ . '/../resources/dist/components/socialment.js'),
-            // Css::make('socialment-styles', __DIR__ . '/../resources/dist/socialment.css'),
-            // Js::make('socialment-scripts', __DIR__ . '/../resources/dist/socialment.js'),
-        ];
-    }
+	/**
+	 * @return array<Asset>
+	 */
+	protected function getAssets(): array
+	{
+		return [
+			// AlpineComponent::make('socialment', __DIR__ . '/../resources/dist/components/socialment.js'),
+			// Css::make('socialment-styles', __DIR__ . '/../resources/dist/socialment.css'),
+			// Js::make('socialment-scripts', __DIR__ . '/../resources/dist/socialment.js'),
+		];
+	}
 
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            SocialmentCommand::class,
-        ];
-    }
+	/**
+	 * @return array<class-string>
+	 */
+	protected function getCommands(): array
+	{
+		return [];
+	}
 
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
+	/**
+	 * @return array<string>
+	 */
+	protected function getIcons(): array
+	{
+		return [];
+	}
 
-    /**
-     * @return array<string>
-     */
-    // protected function getRoutes(): array
-    // {
-    //     return [];
-    // }
+	/**
+	 * @return array<string>
+	 */
+	// protected function getRoutes(): array
+	// {
+	//     return [];
+	// }
 
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
+	/**
+	 * @return array<string, mixed>
+	 */
+	protected function getScriptData(): array
+	{
+		return [];
+	}
 
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_connected_accounts_table',
-        ];
-    }
+	/**
+	 * @return array<string>
+	 */
+	protected function getMigrations(): array
+	{
+		return [
+			'create_connected_accounts_table',
+		];
+	}
 }
