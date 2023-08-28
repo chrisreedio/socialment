6093958a8fdb3158a9debdaad46db25279cffbbe
Chris Reed <chris@reedtech.us>
2023-08-28 05:40:26 -0500

    Hard coded Azure provider for now in blade

--- resources/views/providers-list.blade.php
+++ resources/views/providers-list.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class='flex justify-center p-2'>
         <a class='ring-2 ring-slate-700/50 hover:ring-slate-600/70 transition-all rounded-lg px-4 py-3 flex gap-2 items-center'
-            href='{{ route('login.oauth.redirect', 'azure') }}'>
+            href='{{ route('socialment.redirect', 'azure') }}'>
             <x-fab-microsoft class='w-8' />
             <span>Azure Active Directory</span>
         </a>
