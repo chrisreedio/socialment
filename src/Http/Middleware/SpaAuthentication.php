d95ba63482401e0471196a01a133fea1fb40f40e
chrisreedio <chrisreedio@users.noreply.github.com>
2023-12-06 10:47:27 +0000

    Fix styling

--- src/Http/Middleware/SpaAuthentication.php
+++ src/Http/Middleware/SpaAuthentication.php
@@ -8,7 +8,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class SpaAuthentication
 {
