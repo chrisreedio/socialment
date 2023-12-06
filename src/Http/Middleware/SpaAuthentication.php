5e0c44070b9991db8ff0fa2f4696c0902a4ae6e3
Chris Reed <chris@reedtech.us>
2023-12-06 04:46:45 -0600

    feat(spa-auth): streamline SPA guard handling #save-previous-url
    Refactor `SpaAuthentication` middleware to accept a guard parameter.
    This change simplifies auth checks, allowing dynamic guard assignment.
    Removed redundant comments and outdated response handling.
    Improved API error response for unauthenticated access in SPA context.

--- src/Http/Middleware/SpaAuthentication.php
+++ src/Http/Middleware/SpaAuthentication.php
@@ -17,45 +17,13 @@ class SpaAuthentication
      *
      * @param  Closure(Request): (Response|RedirectResponse)  $next
      */
-    public function handle(Request $request, Closure $next): Response | RedirectResponse | JsonResponse
+    public function handle(Request $request, Closure $next, ?string $guard = 'spa'): Response | RedirectResponse | JsonResponse
     {
-        $spaLoggedIn = Auth::guard('spa')->check();
-
-        // Log::info('SpaAuthentication Middleware', [
-        //     'spaLoggedIn' => $spaLoggedIn,
-        //     'request' => $request->all(),
-        // ]);
-        // return response()
-        //     ->json([
-        //             'spaLoggedIn' => $spaLoggedIn,
-        //         ]
-        //     );
-
-        // dd("SPA LOGGED IN: " . ($spaLoggedIn ? "TRUE" : "FALSE"));
-        if (! Auth::guard('spa')->check()) {
-            // Log::error('Spa 401!', [
-            // 	'request' => $request->all(),
-            // 	'response' => $response,
-            // ]);
-
-            // Old method: Redirect to a hard 401 error page, bad UX
-            abort(401, 'Not logged in');
-
-            // Option #2: Redirect to the 'home page' - Probably the best option
-            // We could optionally pass a message by 'flash'ing it to the session
-            // return redirect()
-            // ->to('https://nuxt-chat.local.winux.io:3000/login')
-            // ->withErrors('You must be logged in to access this page.');
-
-            // Option #3: Just display the pretty error page on the existing URL.
-            // This is not a very elegant solution, but it works.
-            // return inertia('Error/Landing', [
-            // 	'title' => 'Not Logged In',
-            // 	'message' => "We're sorry, but your session has ended due to inactivity. To ensure the security of your information, we automatically log you out after a period of time. Please log in again to continue.",
-            // ]);
+        if (! Auth::guard($guard)->check()) {
+            return response()->json(['message' => 'Not logged in'], 401);
         }
 
-        // If we get here, the spa user is logged in
+        // If we get here, the user is logged in
         return $next($request);
     }
 }
