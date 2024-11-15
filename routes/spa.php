0e5264573e4938e268cacf60caf6e1242723ff9e
chrisreedio <chrisreedio@users.noreply.github.com>
2024-11-15 20:32:22 +0000

    Fix styling

--- routes/spa.php
+++ routes/spa.php
@@ -3,14 +3,7 @@
 use ChrisReedIO\Socialment\Http\Controllers\CsrfCookieController;
 use ChrisReedIO\Socialment\Http\Controllers\SocialmentController;
 use ChrisReedIO\Socialment\Http\Controllers\SpaAuthController;
-use ChrisReedIO\Socialment\Http\Middleware\VerifySpaCsrfToken;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 // Custom SPA specific route for getting a CSRF cookie
 Route::get('sanctum/csrf-cookie', [CsrfCookieController::class, 'show'])->name('csrf-cookie');
@@ -21,7 +14,7 @@ Route::get('login/{provider}', [SocialmentController::class, 'redirectSpa'])
     ->name('redirect');
 // Authenticated Routes
 Route::middleware(['auth:sanctum'])->group(function () {
-// Route::middleware(['auth'])->group(function () {
+    // Route::middleware(['auth'])->group(function () {
     Route::post('logout', [SpaAuthController::class, 'logout'])->name('logout');
     Route::get('me', [SpaAuthController::class, 'me'])->name('me');
 });
