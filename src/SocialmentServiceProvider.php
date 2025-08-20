169e1e937f81284e623628b3da1cf744d98c41dc
chrisreedio <77644584+chrisreedio@users.noreply.github.com>
2025-08-20 20:13:38 +0000

    Fix styling

--- src/SocialmentServiceProvider.php
+++ src/SocialmentServiceProvider.php
@@ -58,7 +58,7 @@ class SocialmentServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
-        $this->app->singleton(SocialmentPlugin::class, fn () => new SocialmentPlugin());
+        $this->app->singleton(SocialmentPlugin::class, fn () => new SocialmentPlugin);
     }
 
     public function packageBooted(): void
@@ -109,7 +109,7 @@ class SocialmentServiceProvider extends PackageServiceProvider
         }
 
         // Testing
-        Testable::mixin(new TestsSocialment());
+        Testable::mixin(new TestsSocialment);
     }
 
     protected function getAssetPackageName(): ?string
